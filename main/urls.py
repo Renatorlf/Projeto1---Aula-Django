954c4db10b963874bdf35ecc2a9680177a4ea52b
Renato Lopes de Freitas <renato.tec.lopes@gmail.com>
2023-08-26 17:00:52 -0300

    modificado 26/08/23

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.alunoView, name='aluno-lista'),
+    path('', views.alunoView, name='aluno-lista'), #url da primeira pagina
+    
+    path('alunoID/<int:id>', views.alunoIDview, name='aluno-detalhe'), #url da segunda pagina
 ]
